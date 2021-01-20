08bb6968780a2ae67bccb6fac5f5dd87a4f3d4f6
wildanGhifari <wildghifari@gmail.com>
2021-01-20 16:50:43 +0700

    Create project section

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,10 @@
-const serviceCard = document.querySelector('#services .service-summary .card');
-const moreInfo = document.querySelectorAll('.moreInfo');
+const cardTransform = () => {
+    const serviceCard = document.querySelector('#services .service-summary .card');
+    const moreInfo = document.querySelectorAll('#services .service-summary .card .moreInfo');
+
+    // serviceCard.addEventListener('mouseover', () => {
+    //     moreInfo.innerHTML = 'Test';
+    // });
+}
+
+cardTransform();
