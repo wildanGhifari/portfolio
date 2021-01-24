a67c5850c59f95ec5acfc5fcd83bd210af8bee8e
wildanGhifari <wildghifari@gmail.com>
2021-01-25 00:09:34 +0700

    Update on navbar, welcome, service and projects section

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,10 @@
 const cardTransform = () => {
     const serviceCard = document.querySelector('#services .service-summary .card');
     const moreInfo = document.querySelectorAll('#services .service-summary .card .moreInfo');
+    const cardImg = document.querySelectorAll('#services .service-summary .card img')
 
     // serviceCard.addEventListener('mouseover', () => {
-    //     moreInfo.innerHTML = 'Test';
+        
     // });
 }
 
